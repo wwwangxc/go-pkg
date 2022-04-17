92111d5525f49889582524d53aaf72f4a258f021
wwwangxc <w.xingchen@outlook.com>
2022-04-17 18:18:42 +0800

    feat: update config

--- orm/config_test.go
+++ orm/config_test.go
@@ -7,21 +7,48 @@ import (
 )
 
 func TestInit(t *testing.T) {
-	mysqlCfg, exist := clientConfigMap["db_mysql"]
+	cfg, exist := clientConfigMap["db_mysql"]
 	assert.True(t, exist, "db_mysql should exist")
-	assert.Equal(t, "db_mysql", mysqlCfg.Name)
-	assert.Equal(t, "root:root@tcp(127.0.0.1:3306)/db1?charset=utf8&parseTime=True", mysqlCfg.DSN)
-	assert.Equal(t, "mysql", mysqlCfg.Driver)
-	assert.Equal(t, 111, mysqlCfg.MaxIdle)
-	assert.Equal(t, 222, mysqlCfg.MaxOpen)
-	assert.Equal(t, 333, mysqlCfg.MaxIdleTime)
+	assert.Equal(t, "db_mysql", cfg.Name)
+	assert.Equal(t, "root:root@tcp(127.0.0.1:3306)/db1?charset=utf8&parseTime=True", cfg.DSN)
+	assert.Equal(t, "mysql", cfg.Driver)
+	assert.Equal(t, 111, cfg.MaxIdle)
+	assert.Equal(t, 222, cfg.MaxOpen)
+	assert.Equal(t, 333, cfg.MaxIdleTime)
 
-	postgresqlCfg, exist := clientConfigMap["db_postgresql"]
+	cfg, exist = clientConfigMap["db_postgresql"]
 	assert.True(t, exist, "db_postgresql should exist")
-	assert.Equal(t, "db_postgresql", postgresqlCfg.Name)
-	assert.Equal(t, "user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai", postgresqlCfg.DSN)
-	assert.Equal(t, "postgresql", postgresqlCfg.Driver)
-	assert.Equal(t, 11, postgresqlCfg.MaxIdle)
-	assert.Equal(t, 22, postgresqlCfg.MaxOpen)
-	assert.Equal(t, 33, postgresqlCfg.MaxIdleTime)
+	assert.Equal(t, "db_postgresql", cfg.Name)
+	assert.Equal(t, "user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai", cfg.DSN)
+	assert.Equal(t, "postgresql", cfg.Driver)
+	assert.Equal(t, 11, cfg.MaxIdle)
+	assert.Equal(t, 22, cfg.MaxOpen)
+	assert.Equal(t, 33, cfg.MaxIdleTime)
+
+	cfg, exist = clientConfigMap["db_sqlite"]
+	assert.True(t, exist, "db_sqlite should exist")
+	assert.Equal(t, "db_sqlite", cfg.Name)
+	assert.Equal(t, "go-pkg.db", cfg.DSN)
+	assert.Equal(t, "sqlite", cfg.Driver)
+	assert.Equal(t, 11, cfg.MaxIdle)
+	assert.Equal(t, 22, cfg.MaxOpen)
+	assert.Equal(t, 33, cfg.MaxIdleTime)
+
+	cfg, exist = clientConfigMap["db_sqlserver"]
+	assert.True(t, exist, "db_sqlserver should exist")
+	assert.Equal(t, "db_sqlserver", cfg.Name)
+	assert.Equal(t, "sqlserver://gorm:LoremIpsum86@localhost:9930?database=gorm", cfg.DSN)
+	assert.Equal(t, "sqlserver", cfg.Driver)
+	assert.Equal(t, 11, cfg.MaxIdle)
+	assert.Equal(t, 22, cfg.MaxOpen)
+	assert.Equal(t, 33, cfg.MaxIdleTime)
+
+	cfg, exist = clientConfigMap["db_clickhouse"]
+	assert.True(t, exist, "db_clickhouse should exist")
+	assert.Equal(t, "db_clickhouse", cfg.Name)
+	assert.Equal(t, "tcp://localhost:9000?database=gorm&username=gorm&password=gorm&read_timeout=10&write_timeout=20", cfg.DSN)
+	assert.Equal(t, "clickhouse", cfg.Driver)
+	assert.Equal(t, 11, cfg.MaxIdle)
+	assert.Equal(t, 22, cfg.MaxOpen)
+	assert.Equal(t, 33, cfg.MaxIdleTime)
 }
