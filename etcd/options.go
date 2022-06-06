f949035fc2df9f9b99aa6c3a591569162495a0bc
Xingchen Wang <w.xingchen@outlook.com>
2022-06-06 10:23:27 +0800

    feat: etcd client (#18)

---
+++ etcd/options.go
@@ -0,0 +1,57 @@
+package etcd
+
+// ClientOption etcd client proxy option
+type ClientOption func(*serviceConfig)
+
+// WithTarget set target
+//
+// Target is a list of URLs, multiple URL split by ','
+func WithTarget(target string) ClientOption {
+	return func(sc *serviceConfig) {
+		sc.Target = target
+	}
+}
+
+// WithTimeout set timeout
+//
+// Unit millisecond, default 3000
+func WithTimeout(timeout int) ClientOption {
+	return func(sc *serviceConfig) {
+		sc.Timeout = timeout
+	}
+}
+
+// WithUsername set user name for authentication
+func WithUsername(username string) ClientOption {
+	return func(sc *serviceConfig) {
+		sc.Username = username
+	}
+}
+
+// WithPassword set password for authentication
+func WithPassword(password string) ClientOption {
+	return func(sc *serviceConfig) {
+		sc.Password = password
+	}
+}
+
+// WithTLSKeyPath set tls key file path
+func WithTLSKeyPath(tlsKeyPath string) ClientOption {
+	return func(sc *serviceConfig) {
+		sc.TLSKeyPath = tlsKeyPath
+	}
+}
+
+// WithTLSKeyPath set tls cert file path
+func WithTLSCertPath(tlsCertPath string) ClientOption {
+	return func(sc *serviceConfig) {
+		sc.TLSCertPath = tlsCertPath
+	}
+}
+
+// WithCACertPath set ca cert file path
+func WithCACertPath(caCertPath string) ClientOption {
+	return func(sc *serviceConfig) {
+		sc.CACertPath = caCertPath
+	}
+}
